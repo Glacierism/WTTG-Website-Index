0de11e56d9a71a5d150921902d8920a75b714e7f
Glacierism <glacierisme@gmail.com>
2026-03-02 23:26:19 -0600

    The hole issue
    having issues with the hole. Check the code to see what.

--- Sites/WTTG3/thehole/js/script.js
+++ Sites/WTTG3/thehole/js/script.js
@@ -1,8 +1,8 @@
-var currentVid = document.getElementById('watch');
+var currentVid = document.getElementById('watch'); // issue is its stuck on zoom, no idea how to fix this. Might have to not do this one properly or clone it several times so there is a different link for each one.
 var collection = [
     "img/breathe.webm", 
     "img/pump.webm",
-    "img/heart.webm"
+    "img/heart.webm" 
 ];
 
 var sfx = [
@@ -13,18 +13,21 @@ var sfx = [
 
 function PlayVideo(VideoIndex){
     currentVid.src = collection[VideoIndex];
-    currentVid.play();
+    currentVid.play(img/pump.webm);
 
-    if(currentVid.src.includes("img/breathe.webm")){
+    if(currentVid.src.includes("")){ // i have no idea what the fuck im doing i have never touched JS in my life i should come back to this later 3/2/26
         return;
     }
 
     document.getElementById("overlay").setAttribute("hidden", "");
 }
 
-function ZoomClick(){
-    document.getElementById("watch").setAttribute("src", "img/ZOOM.webm");
-    document.getElementById("watch").removeAttribute("loop");
-    document.getElementById("overlay").setAttribute("hidden", "");
-    sendEventToUnreal("PlaySFX", { SFX: 'Zoom'});
+function ZoomClick(){ // i changed this to my own code but it didnt do anything different. The sites love to talk to unreal which makes sense but pisses me off. 
+    var watchVid = document.getElementById("watch");
+    var overlay = document.getElementById("overlay");
+
+    watchVid.setAttribute("src", "img/ZOOM.webm"); 
+    watchVid.play();
+
+    overlay.setAttribute("hidden", "");
 }
